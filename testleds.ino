668170134c46acea9d1adc31d410f9a9045d23bc
Omkar <42245836+Omkar-C@users.noreply.github.com>
2019-03-10 00:49:14 +0530

    Test Led

---
+++ testleds.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite(2,HIGH);
+  delay(10);
+  digitalWrite(2,LOW);
+  delay(10);
+  
+}
