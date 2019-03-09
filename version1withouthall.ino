e62ff24888f3782114d069db8f14ebdead8c06d4
Omkar <42245836+Omkar-C@users.noreply.github.com>
2019-03-10 00:51:10 +0530

    Version 1 is without hall sensor

---
+++ version1withouthall.ino
@@ -0,0 +1,146 @@
+const bool A[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1};
+const bool B[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,1,1,1,1,1,1,1};
+const bool C[]={1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1};
+const bool D[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,0,1,1,1,1,1,1,0};
+const bool E[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1};
+const bool F[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0};
+const bool G[]={0,1,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,1,1,1};
+const bool H[]={1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1};
+const bool I[]={1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1};
+const bool J[]={1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0};
+const bool K[]={1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0};
+const bool L[]={1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1};
+const bool M[]={1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1};
+const bool N[]={1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1};
+const bool O[]={0,1,1,1,1,1,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0};
+const bool P[]={1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1,0,0,0};
+const bool Q[]={1,1,1,1,1,1,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0};
+const bool R[]={1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0,1,0,1,1,1,1,0,0,0,1};
+const bool S[]={1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,1,1,1};
+const bool T[]={1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0};
+const bool U[]={1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1};
+const bool V[]={1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0,0};
+const bool W[]={1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1};
+const bool X[]={1,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1};
+const bool Y[]={1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0};
+const bool Z[]={0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0};
+const bool Space[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+void setup() {
+  // put your setup code here, to run once:
+
+  //initialize all d2-d11 pins as output
+  for(int i = 2 ; i <= 11; i++)
+  {
+     pinMode(i,OUTPUT);
+  }
+
+  //initialize d12 as input
+  pinMode(12,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+// above red and bottom always on
+digitalWrite(2,HIGH);
+digitalWrite(11,HIGH);
+  // test to be displayed
+  String text = "HELLO WORLD";
+  int length = text.length();
+  display(text);
+  for(int i = 3 ; i < 11; i++)
+  {
+    digitalWrite(i,LOW);
+  }
+ //delay(5);
+}
+
+void display(String text)
+{
+  for( int i = 0; i <= text.length(); i++)
+  {
+     displayLetter(text[i]);
+   //  delay(10);
+  }
+}
+
+void displayLetter(char n)
+{ 
+    switch(n)
+    {
+       case 'A' : Alphadisplay(A);
+                  break;
+       case 'B' : Alphadisplay(B);
+                  break;
+       case 'C' : Alphadisplay(C);
+                  break;
+       case 'D' : Alphadisplay(D);
+                  break;          
+       case 'E' : Alphadisplay(E);
+                  break;
+       case 'F' : Alphadisplay(F);
+                  break;
+       case 'G' : Alphadisplay(G);
+                  break;
+       case 'H' : Alphadisplay(H);
+                  break;
+       case 'I' : Alphadisplay(I);
+                  break;          
+       case 'J' : Alphadisplay(J);
+                  break;    
+       case 'K' : Alphadisplay(K);
+                  break;
+       case 'L' : Alphadisplay(L);
+                  break;
+       case 'M' : Alphadisplay(M);
+                  break;
+       case 'N' : Alphadisplay(N);
+                  break;          
+       case 'O' : Alphadisplay(O);
+                  break;
+       case 'P' : Alphadisplay(P);
+                  break;
+       case 'Q' : Alphadisplay(Q);
+                  break;
+       case 'R' : Alphadisplay(R);
+                  break;
+       case 'S' : Alphadisplay(S);
+                  break;          
+       case 'T' : Alphadisplay(T);
+                  break;    
+       case 'U' : Alphadisplay(U);
+                  break;
+       case 'V' : Alphadisplay(V);
+                  break;
+       case 'W' : Alphadisplay(W);
+                  break;
+       case 'X' : Alphadisplay(X);
+                  break;          
+       case 'Y' : Alphadisplay(Y);
+                  break;
+       case 'Z' : Alphadisplay(Z);
+                  break;
+       case ' ' : Alphadisplay(Space);
+                  break;
+       default : break;
+                  
+    }
+}
+
+void Alphadisplay(bool alphabet[])
+{
+  int counter = 0;
+  for(int i = 0 ; i < 5 ; i++)
+  {
+    for(int j = 3 ; j <=10 ; j++)
+    {
+      digitalWrite(j,alphabet[counter++]);
+      delay(10);
+    }
+    for(int j = 3 ; j <=10; j++)
+    {
+      digitalWrite(j,LOW);
+    }
+  }
+}
