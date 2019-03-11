391b02520150d93c71494662fb8a52658de6afe7
Omkar <42245836+Omkar-C@users.noreply.github.com>
2019-03-11 19:32:48 +0530

    Version 2 with hall sensor
    Support for only Upper case Alphabets

---
+++ Version2/Version2.ino
@@ -0,0 +1,154 @@
+bool A[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1};
+bool B[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,1,1,1,1,1,1,1};
+bool C[]={1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1};
+bool D[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,0,1,1,1,1,1,1,0};
+bool E[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1,1,0,0,1,0,0,0,1};
+bool F[]={1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0};
+bool G[]={0,1,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,1,1,1};
+bool H[]={1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1,1,1,1,1,1};
+bool I[]={1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1};
+bool J[]={1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0};
+bool K[]={1,1,1,1,1,1,1,1,0,0,1,0,1,0,0,0,0,1,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0};
+bool L[]={1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1};
+bool M[]={1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1};
+bool N[]={1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1};
+bool O[]={0,1,1,1,1,1,1,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0};
+bool P[]={1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,1,1,1,1,0,0,0};
+bool Q[]={1,1,1,1,1,1,1,0,1,0,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0};
+bool R[]={1,1,1,1,1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,1,0,1,0,0,1,0,0,1,0,0,1,0,1,1,1,1,0,0,0,1};
+bool S[]={1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1,1,0,0,0,1,1,1,1};
+bool T[]={1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0};
+bool U[]={1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1};
+bool V[]={1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,1,1,1,0,0};
+bool W[]={1,1,1,1,1,1,1,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1};
+bool X[]={1,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,1};
+bool Y[]={1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0};
+bool Z[]={0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0};
+bool Space[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+
+int timer = 500;
+int SensorPin = 12;
+
+void setup() {
+
+  //initialize all d2-d11 pins as output
+  for(int pin = 2 ; pin <= 11; pin++)
+  {
+     pinMode(pin,OUTPUT);
+  }
+
+  //initialize d12 as input
+  pinMode(SensorPin,INPUT);
+}
+
+void loop() {
+
+// above red and bottom always on
+digitalWrite(2,HIGH);
+digitalWrite(11,HIGH);
+
+  // test to be displayed
+  String text = "APNA TIME AA GAYA";
+  int length = text.length();
+  
+  int state = digitalRead(SensorPin);
+  if(state == 0)
+  {
+    display(text);
+  }
+  
+  
+}
+
+void display(String text)
+{
+  for( int i = 0; i <= text.length(); i++)
+  {
+     displayLetter(text[i]);
+  }
+}
+
+void displayLetter(char n)
+{ 
+    switch(n)
+    {
+       case 'A' : Alphadisplay(A);
+                  break;
+       case 'B' : Alphadisplay(B);
+                  break;
+       case 'C' : Alphadisplay(C);
+                  break;
+       case 'D' : Alphadisplay(D);
+                  break;          
+       case 'E' : Alphadisplay(E);
+                  break;
+       case 'F' : Alphadisplay(F);
+                  break;
+       case 'G' : Alphadisplay(G);
+                  break;
+       case 'H' : Alphadisplay(H);
+                  break;
+       case 'I' : Alphadisplay(I);
+                  break;          
+       case 'J' : Alphadisplay(J);
+                  break;    
+       case 'K' : Alphadisplay(K);
+                  break;
+       case 'L' : Alphadisplay(L);
+                  break;
+       case 'M' : Alphadisplay(M);
+                  break;
+       case 'N' : Alphadisplay(N);
+                  break;          
+       case 'O' : Alphadisplay(O);
+                  break;
+       case 'P' : Alphadisplay(P);
+                  break;
+       case 'Q' : Alphadisplay(Q);
+                  break;
+       case 'R' : Alphadisplay(R);
+                  break;
+       case 'S' : Alphadisplay(S);
+                  break;          
+       case 'T' : Alphadisplay(T);
+                  break;    
+       case 'U' : Alphadisplay(U);
+                  break;
+       case 'V' : Alphadisplay(V);
+                  break;
+       case 'W' : Alphadisplay(W);
+                  break;
+       case 'X' : Alphadisplay(X);
+                  break;          
+       case 'Y' : Alphadisplay(Y);
+                  break;
+       case 'Z' : Alphadisplay(Z);
+                  break;
+       case ' ' : Alphadisplay(Space);
+                  break;
+       default : break;
+                  
+    }
+}
+void clear(int timer)
+{
+  for(int i = 3 ; i<11 ; i++)
+    pinMode(i,LOW);
+  delayMicroseconds(timer);
+}
+void Alphadisplay(bool alphabet[])
+{
+  int counter = 0;
+  for(int state = 0 ; state < 5 ; state++)
+  {
+    for(int pin = 3 ; pin <=10 ; pin++)
+    {
+      digitalWrite(pin,alphabet[counter++]);
+      
+    }
+  
+    delayMicroseconds(timer);
+    clear(900);
+    
+  }
+}
